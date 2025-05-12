18bd0b491fa797d465e3429b408ebb0a42f0a814
Ayse Yildiz <ayseyildizz1923@gmail.com>
2025-05-12 15:27:32 +0300

    Last changings

--- src/CountingBasedAlgorithm.java
+++ src/CountingBasedAlgorithm.java
@@ -2,7 +2,7 @@ public class CountingBasedAlgorithm {
     //This function finds majority element with counting occurrences of numbers and adding them into an array.
     public static int findMajorityCounting(int[] nums) {
             int n = nums.length;
-            int[] count = new int[n];
+            int[] count = new int[2*n];
 
             for (int num : nums) {
                 count[num]++;
